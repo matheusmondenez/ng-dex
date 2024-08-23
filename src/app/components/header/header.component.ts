f37380d868fb0cd75d7ab7b01d171706d93b9b37
Matheus Mondenez Paulino <matheusmondenez@gmail.com>
2024-08-23 20:46:29 -0300

    feat: inits filter by name functions

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 import { ButtonComponent } from '../button/button.component'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, ButtonComponent],
+  imports: [RouterModule, ButtonComponent, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   public label: string = 'Dark'
+  public name: string = ''
+
+  public setSearch(event: any) {
+    event.preventDefault()
+    this.name = event.target.value
+
+    alert(`Set Search: ${event.target.value}`)
+  }
+
+  public getSearch() {
+    alert(`Get Search: ${this.name}`)
+  }
+
+  public filter() {
+    alert('Filter!')
+  }
 
   public toogleTheme() {
     const dark = document.body.classList.toggle('dark')
@@ -20,5 +37,9 @@ export class HeaderComponent {
     } else {
       this.label = 'Dark'
     }
+  }
+
+  public alert(text: string) {
+    alert(text)
   }
 }
